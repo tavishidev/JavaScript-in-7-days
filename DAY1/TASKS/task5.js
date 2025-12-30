24044c635da89d7d75a45743a712ca17d80dfca0
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 14:53:40 +0530

    task 5 completed

---
+++ DAY1/TASKS/task5.js
@@ -0,0 +1,11 @@
+const r=5;
+const d=2*r;
+const circumference=2*3.14*r;
+const area=3.14 * Math.pow(r,2);
+console.log("given radius=",r);
+console.log("circumference =",circumference);
+console.log("area=",area);
+
+
+
+
