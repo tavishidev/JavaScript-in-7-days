9a9634a568e1487a6d726b82f75fb66a92763d6d
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 13:56:03 +0530

    task 3 completed

--- DAY1/TASKS/task3.js
+++ DAY1/TASKS/task3.js
@@ -2,12 +2,21 @@ let name="Tavishi singh";
 let age=19;
 let city="chandigarh";
 const country="India";
-let professsion="student";
+let profession="student";
 console.log(name);
 console.log(age);
 console.log(city);
 console.log(country);
 console.log(profession);
+let birth_yr=2025-age;
+let msg=`my name is ${name}. I'm ${age} years old as my birthyear is ${birth_yr}. I live in ${city},${country}. I am a ${profession} pursing btech in cse AI/ML.`
+console.log(msg);
+
+
+
+
+
+
 
 
 
