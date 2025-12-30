061fc7d720fdc6780a025dfa7c8b44e96e6bc1ef
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 17:31:15 +0530

    task 8 completed

---
+++ DAY1/TASKS/task8.js
@@ -0,0 +1,21 @@
+let x=Math.random();//this will generate random value between 0 and 1 , 1 not included
+let t=Math.floor((x*100)+1);//random value between 1 and 100
+let y=Math.round(x);
+let z=Math.floor(x);
+let w=Math.ceil(x);
+let sqr=Math.pow(t,2);
+let sqr_root=Math.pow(t,1/2);
+console.log("random no from 0 and 1=",x);
+console.log("random no from 1 and 100=",t);
+console.log("rounding it off =",y);
+console.log("floor value",z);
+console.log("ceiling value",w);
+console.log("square of random value=",sqr);
+console.log("square root of random value=",sqr_root);
+
+
+
+
+
+
+
