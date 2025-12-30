e4218aa64911e7ac0d50eca33d91b371d16e0465
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 19:18:15 +0530

    task 10 completed

---
+++ DAY1/TASKS/task10.js
@@ -0,0 +1,15 @@
+const P=1000;
+const R=9;
+const T=2;
+const SI=(P*R*T)/100;
+const x=P*(1+R/100);
+const CI=Math.pow(x,2);
+const diff=CI-SI;
+console.log("FINANCIAL SUMMARY:");
+console.log("SI=",SI);
+console.log("CI=",CI);
+
+
+
+
+
