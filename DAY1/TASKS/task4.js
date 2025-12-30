24927ebc724e2261d43e3d9535b7584117b14241
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 14:42:32 +0530

    rounding of F_TO_C TO get the accurate value of celcius temp with decimal.

--- DAY1/TASKS/task4.js
+++ DAY1/TASKS/task4.js
@@ -4,12 +4,8 @@ const change_to_kelvin=celsius_temp + 273.15;
 console.log("given celcius temp=",celsius_temp);
 console.log("after converting celsius temperature to farhrenheit the value is=", change_to_fahrenheit);
 console.log("aftter converting celcius temperature to kelvin the value is=",change_to_kelvin);
-const F_TO_C=((change_to_fahrenheit-32)*5)/9.0;
+const F_TO_C=((change_to_fahrenheit-32)*5)/9;
 const K_TO_C=change_to_kelvin-273.15;
-console.log("REVERSE F TO C=",F_TO_C);
+console.log("REVERSE F TO C=",Math.round(F_TO_C));
 console.log("reverse K TO C=",K_TO_C);
-
-
-
-
 
