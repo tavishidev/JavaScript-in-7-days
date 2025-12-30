4740aa2346f7893ff3ce39db13e2f80c63c97ad0
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 18:45:16 +0530

    task 9 committed

---
+++ DAY1/TASKS/task9.js
@@ -0,0 +1,44 @@
+let x=7;
+let y="hello";
+let z=8987598951972n;
+let t;
+let w=null;
+let isstudent=true;
+const id=Symbol("id");
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof t);
+console.log(typeof w);
+console.log(typeof isstudent);
+console.log(typeof id);
+
+//string to number conversion
+let str="5";
+console.log("str=",str);
+console.log(typeof str);
+console.log(typeof parseInt(str));
+
+//number to string conversion
+let num=67;
+let text=String(num);
+console.log("num=",num);
+console.log(typeof num);
+console.log("text=",text);
+console.log(typeof text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
