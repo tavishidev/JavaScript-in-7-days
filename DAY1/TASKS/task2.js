f62e238a9c533e765fbbef39376a843163865ad6
tavishidev <tavishichauhan006@gmail.com>
2025-12-30 12:14:18 +0530

    task 1 completed

--- DAY1/TASKS/task2.js
+++ DAY1/TASKS/task2.js
@@ -1,8 +1,67 @@
-// Declare 5 variables using let with different data types
+/* Declare 5 variables using let with different data types
 let name="tavishidev";
 let age=18;
+let a;
+let b=null;
+let num=123456789n;
 console.log(name);
 console.log(age);
+console.log(a);
+console.log(b);
+console.log(num);*/
+
+
+// declare 5 constants using const
+/*const mood="happy";
+const myno=8987588957n;
+const myid=112830;
+console.log(mood);
+console.log(myno);
+console.log(myid);*/
+
+
+ //try to reassign a const and observe the error
+ /*const mood="angry";
+ const myno=8987598932n;
+ const myid=118720;
+ console.log(mood);
+ console.log(myno);
+ console.log(myid);*/
+ //this will show error 
+
+ 
+ //create example showing difference between let and var
+//difference- var is not block scoped whereas let is.
+/*eg for var
+{
+    console.log(a);
+}
+var a=3;
+
+//eg for let
+{
+    console.log(b);
+}
+let b=4;*/
+
+
+ //:hoisting behavior-
+
+ //console.log(user);
+ ///let user="tavishi"; //it will show error as it need to be initialize before using
+ console .log(author);
+ var author="NARAYAN"; //undefined: engines knows that author exist 
+
+
+ 
+
+
+
+
+
+
+
+
 
 
 
---
+++ DAY1/TASKS/task3.js
@@ -0,0 +1,15 @@
+let name="Tavishi singh";
+let age=19;
+let city="chandigarh";
+const country="India";
+let professsion="student";
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(country);
+console.log(profession);
+
+
+
+
+
