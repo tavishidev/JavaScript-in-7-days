9fdb43f6beeaa2c283c9629086207f9248d976e7
tavishidev <tavishichauhan006@gmail.com>
2026-01-01 02:37:53 +0530

    task 4 completed

---
+++ DAY2/TASKS/task4.js
@@ -0,0 +1,36 @@
+let day_no = 98;
+let event = "sports event";
+console.log("activity for each day=", event);
+switch (day_no) {
+    case 1:
+        console.log("monday");
+        console.log("sports= badminton competition ");
+        break;
+    case 2:
+        console.log("tueday");
+        console.log("sports= cricket competition");
+        break;
+    case 3:
+        console.log("wednesday");
+        console.log("sports= vollyball competition");
+        break;
+    case 4:
+        console.log("thursday");
+        console.log("sports= basketball competition");
+        break;
+    case 5:
+        console.log("friday");
+        console.log("sports= chess competition");
+        break;
+    case 6:
+        console.log("saturday");
+        console.log("sports= caroom competition");
+        break;
+    case 7:
+        console.log("sunday");
+        console.log("sports=hockey competition");
+        break;
+    default:
+        console.log("invalid number: give no. from 1-7");
+            
+}
