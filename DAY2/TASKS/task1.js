08421c922e9106ebaf84684475c35cc138da393d
tavishidev <tavishichauhan006@gmail.com>
2025-12-31 13:58:26 +0530

    task 1 completed

---
+++ DAY2/TASKS/task1.js
@@ -0,0 +1,31 @@
+let x = 999;
+if (x > 0)
+    console.log("number is positive");
+
+else if (x === 0)
+    console.log("number is zero")
+
+else
+    console.log("number is negative");
+
+//checking even/odd
+if (x % 2 === 0)
+    console.log("number is even");
+else
+    console.log("number is odd");
+
+// Check if it's a single digit, double digit, or more 
+if (x >= -9 && x <= 9)
+    console.log("number is single digit");
+else if (x >= -99 && x <= 99)
+    console.log("number is double digit");
+else 
+    console.log("number is more than double digit");
+
+
+
+
+
+
+
+
