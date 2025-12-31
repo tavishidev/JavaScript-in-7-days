812034cdace63dacd6f8ae5b2af7e8f72b2440d8
tavishidev <tavishichauhan006@gmail.com>
2025-12-31 14:29:07 +0530

    task 2 completed

---
+++ DAY2/TASKS/task2.js
@@ -0,0 +1,38 @@
+//for yr =2000
+let yr1=2000;
+console.log("yr1=2000");
+if(yr1%4===0 && yr1%100!=0)
+    console.log("it is a leap year");
+else
+    console.log("not a leap year");
+
+// for yr=2020
+console.log("yr2=2020");
+let yr2=2020;
+if(yr2%4===0 && yr2%100!=0)
+    console.log("it is a leap year");
+else
+    console.log("not a leap year");
+
+
+//for yr=1900
+let yr3=1900;
+console.log("yr3=1900");
+if(yr3%4===0 && yr3%100!=0)
+    console.log("it is a leap year");
+else
+    console.log("not a leap year");
+
+
+//for yr=2024
+let yr4=2024;
+console.log("yr4=2024");
+
+if(yr4%4===0 && yr4%100!=0)
+    console.log("it is a leap year");
+else
+    console.log("not a leap year");
+
+
+    
+    
