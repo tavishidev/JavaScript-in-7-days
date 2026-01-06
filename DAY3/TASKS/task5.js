b19c8c5bbb3760708234495e9b2dcb553fc05b63
tavishidev <tavishichauhan006@gmail.com>
2026-01-06 23:34:37 +0530

    task5 completed

---
+++ DAY3/TASKS/task5.js
@@ -0,0 +1,69 @@
+//  Check if a single number is prime 
+/*let num1 = 9;
+let count = 0;
+for (let i = 2; i < num1; i++) {
+    if (num1 % i === 0)
+        count = count + 1;
+}
+if (num1 === 0 || num1 === 1)
+    console.log("number is neither prime nor composite");
+else if (count > 0)
+    console.log("number is not prime")
+else
+    console.log("number is prime");*/
+
+
+//Find all prime numbers between 1 and 100 AND  count the number of times prime exist between 1 and 100
+/*console.log("all prime numbers between 1 and 100 is:");
+let count1 = 0;
+for (let num = 2; num <= 100; num++) {
+    let IsPrime = true;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            IsPrime = false
+            break;
+        }
+    }
+    if (IsPrime) {
+        console.log(num);
+        count1 = count1 + 1;
+    }
+
+}
+console.log(`the number of times prime exist between 1 and 100 is ${count1}`)*/
+
+
+// Find the first N prime numbers (e.g., first 20 primes
+let Count=0;
+for (let number = 2; number <= 100; number++) {
+    let isprime = true;
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            isprime = false;
+            break;
+        }
+        if (isprime) {
+            Count = Count + 1;
+            console.log(number);
+
+            if (Count === 20)
+                break;
+            }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
