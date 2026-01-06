46f22c6d7edad527bcefffa18d8a6032aea50592
tavishidev <tavishichauhan006@gmail.com>
2026-01-07 00:04:53 +0530

    task 6 completed

---
+++ DAY3/TASKS/task6.js
@@ -0,0 +1,17 @@
+// Print a right triangle of stars:
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+//Print an inverted right triangle
+for (let i = 5; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
