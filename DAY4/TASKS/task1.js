612dec168cec19b0445a0593c6c1153e7b4a2e1c
tavishidev <tavishichauhan006@gmail.com>
2026-01-08 01:03:02 +0530

    task 1 completed

---
+++ DAY4/TASKS/task1.js
@@ -0,0 +1,38 @@
+//Create a function that takes a name and returns a greeting
+function userName(name){
+    return `goodmorning ${name}`;
+}
+console.log(userName("tavishi"));
+
+// Create a function that takes two numbers and returns their sum 
+function addUser(a,b){
+    return a+b;
+}
+console.log(addUser(4,5));
+
+//Create a function that calculates the area of a rectangle
+function areaOfRectangle(lenght,breadth) {
+    return lenght*breadth;
+}
+console.log(areaOfRectangle(4,6));
+
+ //Create a function that converts miles to kilometers 
+function mileToKm(a_in_miles){
+    return a_in_miles *1000;
+ }
+    console.log(mileToKm(5));
+
+
+//Create a function that returns the current year 
+function currentYear(){
+    return new Date().getFullYear();
+}
+console.log(currentYear());
+
+    
+
+
+
+
+
+
