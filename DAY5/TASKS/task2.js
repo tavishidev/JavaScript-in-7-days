87790f634a6db9541aff522b3cb17064ba48ee3a
tavishidev <tavishichauhan006@gmail.com>
2026-01-11 02:19:29 +0530

    task 2 completed

---
+++ DAY5/TASKS/task2.js
@@ -0,0 +1,26 @@
+let arr=[1,8,0,7,2];
+arr.push(6);
+console.log(arr);
+//arr.unshift(14);
+//console.log(arr);
+arr.pop();
+console.log(arr);
+//arr.shift();
+//console.log(arr);
+arr.splice(2,0,78);
+console.log(arr);
+arr.splice(1,2);
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
