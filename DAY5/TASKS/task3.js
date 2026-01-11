def97edd18cd9c0b43f671aa79682bd192595264
tavishidev <tavishichauhan006@gmail.com>
2026-01-11 15:56:39 +0530

    task 3 completed

---
+++ DAY5/TASKS/task3.js
@@ -0,0 +1,14 @@
+let arr=["white","black","orange","red","blue"];
+console.log(arr.includes("red"));
+console.log(arr.indexOf("black"));
+
+console.log(arr.every(item => typeof item===String));
+
+
+
+
+
+
+
+
+ 
