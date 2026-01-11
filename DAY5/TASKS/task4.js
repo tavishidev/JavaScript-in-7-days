4865650561b0d1cfd9834c4a8a6b54ab0c7f279f
tavishidev <tavishichauhan006@gmail.com>
2026-01-11 16:02:31 +0530

    task 4 completed

---
+++ DAY5/TASKS/task4.js
@@ -0,0 +1,17 @@
+let oldarr=[3,5,7,8,9];
+newarr=oldarr.map(element=>element*2);
+console.log(newarr);
+
+let name=["siksha","nikita","arushi","tannu"];
+console.log(newname=name.map(element=>element.toUpperCase()));
+
+let price=[20,30,40,50];
+console.log(newprice= price.map(element=>element+element* 10/100));
+
+let num=[45,67,23];
+console.log(newNum=num.map(element=>Math.pow(element,2)));
+
+let words=["rude","chaos","perfect","intelligent"];
+console.log(newWords=words.map(element=>element.length));
+
+
