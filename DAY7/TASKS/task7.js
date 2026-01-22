abc88eca4796cde765f8f8f77ff60e230f923ea0
tavishidev <tavishichauhan006@gmail.com>
2026-01-23 01:00:32 +0530

    task 7 modified

--- DAY7/TASKS/task7.js
+++ DAY7/TASKS/task7.js
@@ -1,3 +1,9 @@
+/*Create 3 Promises that resolve at different times 
+   Use Promise.all() to wait for all 
+   Use Promise.race() to get the fastest 
+   Handle the case when one Promise fails in Promise.all() 
+   Create a timeout wrapper using Promise.race()*/
+
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("Promise 1 resolved");
@@ -6,11 +12,9 @@ const p1 = new Promise((resolve, reject) => {
 
 const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let success=false;
-    if(success)
-         resolve("Promise 2 resolved");
-    else
-        reject("promise 2 failed");
+    let success = false;
+    if (success) resolve("Promise 2 resolved");
+    else reject("promise 2 failed");
   }, 2000);
 });
 
@@ -20,14 +24,42 @@ const p3 = new Promise((resolve, reject) => {
   }, 3000);
 });
 
-
-//
-Promise.all([p1,p2,p3])
-.then((results)=>console.log(results))
-.catch((error)=>console.log(error))
+Promise.all([p1, p2, p3])
+  .then((results) => console.log(results))
+  .catch((error) => console.log("error:", error));
 
 Promise.race([p1, p2, p3])
-  .then(result => console.log(result))
-  .catch(error => console.error(error));
+  .then((result) => console.log(result))
+  .catch((error) => console.error("error:", error));
 
+//Create a timeout wrapper using Promise.race()
+/*Timeout wrapper means a function that wraps another function and adds a time limit to it .
+If the original function doesn’t finish within the given time, the wrapper stops waiting and triggers a timeout action*/
 
+function originalTask() {
+  let timerID;
+  const promise = new Promise((resolve) => {
+    timerID = setTimeout(() => {
+      console.log("hello miss");
+      resolve("original function finished");
+    }, 6000);
+  });
+  return { promise, timerID };
+}
+
+function timeoutTask(ms, timerID) {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      console.log("stop waiting for original function to run");
+      clearTimeout(timerID);
+      reject("original function doesn't finish on time");
+    }, ms);
+  });
+}
+
+const { promise, timerID } = originalTask();
+Promise.race([promise, timeoutTask(4000, timerID)])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => console.log(error));
